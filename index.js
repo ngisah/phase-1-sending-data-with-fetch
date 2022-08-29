13d3e88c7102035a009636242bbdc0666e105856
edwin <ngisahedwin@gmail.com>
2022-08-29 13:11:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+function submitData(name, email) {
+    const body = document.querySelector("body");
+  
+  
+    const submittedData = {
+      name: `${name}`,
+      email: `${email}`
+    };
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(submittedData)
+    };
+  
+  
+    return fetch("http://localhost:3000/users", configObj)
+    .then( response => response.json() )
+    .then( json => {
+      console.log(json);
+      body.append(json.id);
+    })
+    .catch( error => {
+      alert("bad ting");
+      console.log(error);
+      body.append(error);
+    });
+  }
+  
+  function getData(id) {
+    return fetch(`http://localhost:3000/users/${id}`)
+    .then( response => response.json())
+    .then( json => console.log(json))
+    .catch( error => console.log(error));
+  }
+  
